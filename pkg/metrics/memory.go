e7918e50f0090ba4f64c1bd1203ed8e6ade7afac
Daniel <nieltansah@gmail.com>
2019-10-29 16:59:52 +0700

    collector: Extract collector.collectRAMMetrics from collector.CollectMemoryMetrics

--- pkg/metrics/memory.go
+++ pkg/metrics/memory.go
@@ -10,12 +10,21 @@ func (collector *collector) collectMemoryMetrics(
 	containerName string,
 	memState lxdapi.ContainerStateMemory,
 ) {
-	ch <- prometheus.MustNewConstMetric(
-		memUsageDesc, prometheus.GaugeValue, float64(memState.Usage), containerName)
-	ch <- prometheus.MustNewConstMetric(memUsagePeakDesc,
-		prometheus.GaugeValue, float64(memState.UsagePeak), containerName)
+	collector.collectRAMMetrics(ch, containerName, memState)
+
 	ch <- prometheus.MustNewConstMetric(swapUsageDesc,
 		prometheus.GaugeValue, float64(memState.SwapUsage), containerName)
 	ch <- prometheus.MustNewConstMetric(swapUsagePeakDesc,
 		prometheus.GaugeValue, float64(memState.SwapUsagePeak), containerName)
+}
+
+func (collector *collector) collectRAMMetrics(
+	ch chan<- prometheus.Metric,
+	containerName string,
+	memState lxdapi.ContainerStateMemory,
+) {
+	ch <- prometheus.MustNewConstMetric(
+		memUsageDesc, prometheus.GaugeValue, float64(memState.Usage), containerName)
+	ch <- prometheus.MustNewConstMetric(memUsagePeakDesc,
+		prometheus.GaugeValue, float64(memState.UsagePeak), containerName)
 }
